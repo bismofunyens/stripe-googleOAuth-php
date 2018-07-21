ef0c2f6ec484f2c42c86c5e6bd136972a2139804
Wesley Mok <wesleyxmok@gmail.com>
2018-07-20 17:19:28 -0700

    Removed client ID

--- docs/google_client.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$google_client_id = '731971672106-bmglkoauqrhb4mndnmsnipccgrlt2hl9.apps.googleusercontent.com';
-$google_client_secret = 'VKmhu8z04Ss6sj1nXnPO6G5L';
-$google_redirect_url = 'http://wesleymok.ca/nutrigene-task/docs/g-callback.php';
-
-?>
